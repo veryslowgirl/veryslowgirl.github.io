9cb023dfd677f675eeee0c2681e4f0397756b3b4
chazaswag <108348253+chazaswag@users.noreply.github.com>
2024-09-26 16:42:43 +0100

    hi

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 let images = [];
 var honse = false;
-function onInit()
-{
-	for (let n = 1; n <= 67; n++)
-	{
-	images.push("images/random/" + n + ".webp");
-	}
-}
 function horse() {
 	honse = !honse;
 }
@@ -36,43 +29,47 @@ for (let n = 1; n <= 3; n++)
 
 let stars = []; 
 function sparkle() {
-        for (let n = 0; n <= 30; n++) {
-			var width = window.innerWidth;
-			var height = window.innerHeight;
-			var num;
-			if (honse == true )
-			{
-				num = 120;
-			}
-			else
-			{
-				num = 45;
-			}
-            let img = new Image(num);
-			if (honse == true )
-			{
-				img.src = "images/sitestuff/honse.gif";
-			}
-			else
-			{
-				img.src = "images/sitestuff/star.gif";
-			}
-            img.className = "star";
-            img.style.left = Math.floor(Math.random() * width) + "px";
-            img.style.top = Math.floor(Math.random() * height) + "px";
-			let hueRotation = Math.floor(Math.random() * 360);
-            img.style.filter = `hue-rotate(${hueRotation}deg)`;
-            setTimeout(() => {
-					document.getElementById('booty').appendChild(img);
-					stars.push(img); 
-					if (stars.length > 25) {
-						let oldStar = stars.shift(); 
-						oldStar.remove();
-					}
-				}, n * 251);
-                
-				if (n == 25) {
-					setTimeout(() => {sparkle(); }, n * 251);
-                }
-            }
+    var width = window.innerWidth;
+    var height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+    for (let n = 0; n <= 35; n++) {
+        var num;
+        if (honse == true) {
+            num = 120;
+        } else {
+            num = 45;
         }
+
+        let img = new Image(num);
+        img.className = "star";
+        img.style.left = Math.floor(Math.random() * width) + "px";
+        img.style.top = Math.floor(Math.random() * height) + "px";
+        let hueRotation = Math.floor(Math.random() * 360);
+        img.style.filter = `hue-rotate(${hueRotation}deg)`;
+        setTimeout(() => {
+            document.getElementById('booty').appendChild(img);
+            stars.push(img);
+
+            if (honse == true) {
+                img.src = "imgs/sitestuff/honse.gif";
+                img.style.width = "120px";
+                img.style.height = "120px";
+            } else {
+                img.src = "imgs/sitestuff/star.gif";
+                img.style.width = "45px";
+                img.style.height = "45px";
+            }
+
+            if (stars.length > 35) {
+                let oldStar = stars.shift();
+                oldStar.remove();
+            }
+        }, n * 180);
+
+        if (n == 25) {
+            setTimeout(() => {
+                sparkle();
+            }, n * 180);
+        }
+    }
+}
